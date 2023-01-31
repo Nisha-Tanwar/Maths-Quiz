216c77a67223a56da40e49001915536e4c9549e1
Nisha-Tanwar <108987264+Nisha-Tanwar@users.noreply.github.com>
2023-01-31 21:57:17 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,77 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_name").innerHTML=player1_name+":";
+document.getElementById("player2_name").innerHTML=player2_name+":";
+
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+
+document.getElementById("player_question").innerHTML="question turn -"+player1_name;
+document.getElementById("player_answer").innerHTML="answer turn -"+player2_name;
+
+function send()
+{
+    word=document.getElementById("word").value ;
+    newword=word.toLowerCase();
+    char1=newword.charAt(1);
+    lengthdivied2=Math.floor(newword.lenght-1);
+    char2= newword.charAt(lengthdivied2);
+    lenghtminus1=newword.lenght-1;
+    char3=newword.charAt(lenghtminus1);
+    removechar1=newword.replace(char1,"_");
+    removechar2=removechar1.replace(char2,"_");
+    removechar3=removechar2.replace(char3,"_");
+    console.log(removechar3);
+    question_word = "<h4 id='word_display'> Q. "+removechar3+"</h4>";
+     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+      check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+       row = question_word + input_box + check_button ;
+        document.getElementById("output").innerHTML = row;
+         document.getElementById("word").value = "";
+}
+ question_turn="player1";
+ answer_turn="player2";
+
+ function check()
+ {
+    get_answer=document.getElementById("input_check_box").value;
+    answer=get_answer.toLowerCase();
+    if(answer == newword)
+    {
+        if(answer_turn =="player1")
+        {
+            player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML=player1_score;
+        }
+        else
+        { player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML=player2_score;
+        }
+    }
+    if(question_turn=="player1")
+    {
+        question_turn="player2";
+        document.getElementById("player_question").innerHTML="question turn-"+player2_name;
+
+    }
+    else{
+        question_turn="player1";
+        document.getElementById("player_question").innerHTML="question turn-"+player1_name;
+
+    }
+    if(answer_turn=="player1")
+    {
+        answer_turn="player2";
+        document.getElementById("player_answer").innerHTML="answer turn-"+player2_name;
+
+    }
+    else{
+        answer_turn="player1";
+        document.getElementById("player_answer").innerHTML="answer turn-"+player1_name;
+    }
+    document.getElementById("output").innerHTML="";
+}
